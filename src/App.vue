c4ca7363d09dd54bfd7fc1b2dc274e9a73f9c71f
MatyasMa <masek.matyas@seznam.cz>
2024-05-16 15:34:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import input_form from './components/input_form.vue'
+
+export default {
+  name: 'App',
+  components: { input_form }
+}
+</script>
+
+<template>
+  <input_form />
+</template>
+
+<style>
+body {
+  
+  background: #eee;
+
+}
+</style>
